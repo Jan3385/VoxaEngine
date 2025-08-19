701fd296d03dfefb0ff89b1034230844b5a8256f
Hat Dealer <jan.richter2007@gmail.com>
2025-08-19 20:20:05 +0200

    nvm

--- Engine/GameEngine.cpp
+++ Engine/GameEngine.cpp
@@ -58,7 +58,6 @@ void GameEngine::Run(IGame &game, const EngineConfig& config)
     //Main game loop
     while (this->running)
     {
-        std::cout << "test" << std::endl;
         this->StartFrame();
 
         this->Update(game);
