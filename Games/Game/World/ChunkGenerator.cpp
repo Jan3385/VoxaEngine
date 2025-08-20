ef2efa7b12d1c61ebcb099a11bbb456a2a4aacdf
Hat Dealer <jan.richter2007@gmail.com>
2025-08-21 00:59:39 +0200

    renaming for clarity

--- Games/Game/World/ChunkGenerator.cpp
+++ Games/Game/World/ChunkGenerator.cpp
@@ -2,45 +2,45 @@
 
 using namespace Volume;
 
-Chunk* ChunkGenerator::GenerateChunk(const Vec2i &pos){
-    Chunk* chunk = new Chunk(pos);
+Chunk* ChunkGenerator::GenerateChunk(const Vec2i &chunkPos){
+    Chunk* chunk = new Chunk(chunkPos);
 
     for (int x = 0; x < Chunk::CHUNK_SIZE; ++x) {
         for (int y = 0; y < Chunk::CHUNK_SIZE; ++y) {
             //Create voxel determining on its position
-            if (pos.y > 4) {
-                if(pos.y == 5 && y <= 5){
+            if (chunkPos.y > 4) {
+                if(chunkPos.y == 5 && y <= 5){
                     chunk->voxels[y][x] = new VoxelSolid
                         ("Grass", 
-                        Vec2i(x + pos.x * Chunk::CHUNK_SIZE,y + pos.y * Chunk::CHUNK_SIZE), 
+                        Vec2i(x + chunkPos.x * Chunk::CHUNK_SIZE,y + chunkPos.y * Chunk::CHUNK_SIZE), 
                         Temperature(21), 
                         true, 20);
                 }else{
                     chunk->voxels[y][x] = new VoxelSolid
                         ("Dirt", 
-                        Vec2i(x + pos.x * Chunk::CHUNK_SIZE,y + pos.y * Chunk::CHUNK_SIZE), 
+                        Vec2i(x + chunkPos.x * Chunk::CHUNK_SIZE,y + chunkPos.y * Chunk::CHUNK_SIZE), 
                         Temperature(21), 
                         true, 20);
                 }
             }
-            else if (pos.y == 4 && pos.x <= 3) {
-                if(pos.x == 2)
+            else if (chunkPos.y == 4 && chunkPos.x <= 3) {
+                if(chunkPos.x == 2)
                     chunk->voxels[y][x] = new VoxelLiquid
                         ("Water", 
-                        Vec2i(x + pos.x * Chunk::CHUNK_SIZE, y + pos.y * Chunk::CHUNK_SIZE), 
+                        Vec2i(x + chunkPos.x * Chunk::CHUNK_SIZE, y + chunkPos.y * Chunk::CHUNK_SIZE), 
                         Temperature(21), 
                         20);
                 else
                     chunk->voxels[y][x] = new VoxelSolid
                         ("Sand", 
-                        Vec2i(x + pos.x * Chunk::CHUNK_SIZE, y + pos.y * Chunk::CHUNK_SIZE), 
+                        Vec2i(x + chunkPos.x * Chunk::CHUNK_SIZE, y + chunkPos.y * Chunk::CHUNK_SIZE), 
                         Temperature(21), 
                         false, 20);
             }
             else {
                 chunk->voxels[y][x] = new VoxelGas
                     ("Oxygen", 
-                    Vec2i(x + pos.x * Chunk::CHUNK_SIZE, y + pos.y * Chunk::CHUNK_SIZE),
+                    Vec2i(x + chunkPos.x * Chunk::CHUNK_SIZE, y + chunkPos.y * Chunk::CHUNK_SIZE),
                     Temperature(21), 1);
             }
         }
